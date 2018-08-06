a9fd247e854bdb37c80379254542a73067633c06
Samuelimza <osamaazmi1999@gmail.com>
2018-08-07 01:07:10 +0530

    commit

--- backend/file1.py
+++ backend/file1.py
@@ -1,12 +1,12 @@
 import numpy as np
 import cv2
-f=open('path.txt','r')
+f=open('backend/path.txt','r')
 path=f.read()
 img=cv2.imread(path+'\compressed.png',0)
 centroids_index=np.array(img)
 a,b=np.shape(centroids_index)
 centroids_index=np.reshape(centroids_index,[a*b,1],2)
-centroids=np.load('codebook.npy')
+centroids=np.load(path+'\codebook.npy')
 m=a*b
 X_pix=np.zeros((m,3))
 for i in range(0,m):
